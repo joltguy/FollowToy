b18c9d34acd55dd7e990e1110f49dcc8d211028e
Jeff Marlow <jeff@joltguy.com>
2017-03-24 18:16:33 -0400

    Updated to version 1.1
    Updated to support the current version of the Instagram query backend.

--- FollowToy.safariextension/followtoy.js
+++ FollowToy.safariextension/followtoy.js
@@ -5,57 +5,80 @@
 // No warranty is expressed or implied... use at your own risk!
 
 var FollowToy = {
-	version: "FollowToy 1.0",
+	version: "FollowToy 1.1",
 	userId: "",
+	requestToken: "",
 	followers: new Array(),
 	following: new Array(),
+	cursor: "",
 	
 	fetchFollowers: function () {
 		// Guard
 		if (this.userId == "") {
 			return;
 		}
-		var apiCall = "https://www.instagram.com/query?q=ig_user%28" + this.userId + "%29+{%0A++followed_by.first%289999%29+{%0A++++count%2C%0A++++nodes+{%0A++++++id%2C%0A++++++username%0A++++}%0A++}%0A}%0A&ref=relationships%3A%3Afollow_list";
-		var xmlhttp;
+		var apiCall = "https://www.instagram.com/query/";
 		
+		// Previously I could request the entire list at once, now I need to get it page by page.
+		var apiParams = "q=ig_user(" + this.userId + ") ";
+		if (this.cursor == "") {
+			apiParams += "{ followed_by.first(20) { count, page_info, nodes {id,username} } }";
+		} else {
+			apiParams += "{ followed_by.after(" + this.cursor + ",20) { count, page_info, nodes {id,username} } }";
+		}
+		apiParams += "&ref=relationships::follow_list";
+		
+		var xmlhttp;
 		xmlhttp = new XMLHttpRequest();
 		xmlhttp.owner = this;
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 				var data = JSON.parse(xmlhttp.responseText);
-				//println(xmlhttp.responseText);
-				
 				if (data.followed_by != undefined && data.followed_by.nodes.length > 0) {
-					println("Followers: " + data.followed_by.nodes.length);
 					for (i = 0; i<data.followed_by.nodes.length; i++) {
 						var follower = data.followed_by.nodes[i];
 						this.owner.followers.push(follower.username);
 						// println(follower.username + " follows you!");
 					}
-					this.owner.fetchFollowing();
+					if (data.followed_by.page_info.has_next_page) {
+						this.owner.cursor = data.followed_by.page_info.end_cursor;
+						this.owner.fetchFollowers();
+					} else {
+						this.owner.cursor = "";
+						println("Follower count: " + this.owner.followers.length);
+						this.owner.fetchFollowing();
+					}
 				} else {
 					// println("Error: " + xmlhttp.responseText);
 					this.owner.followers.push("--ERROR--");
 				}
 			}
 		};
-		xmlhttp.open("GET", apiCall, true);
-		xmlhttp.send();
+		xmlhttp.open("POST", apiCall, true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.setRequestHeader("X-Instagram-AJAX", "1");
+		xmlhttp.setRequestHeader("X-CSRFToken", this.requestToken);
+		
+		xmlhttp.send(apiParams);
 	},
 	
 	fetchFollowing: function () {
 		// Guard
 		if (this.userId == "") {
 			return;
-		} else if (this.followers.length < 1 || this.following.length > 0) {
-			return;
-		} else if (this.followers[0] == "--ERROR--") {
-			return;
 		}
 		
-		var apiCall = "https://www.instagram.com/query?q=ig_user%28" + this.userId + "%29+{%0A++follows.first%289999%29+{%0A++++count%2C%0A++++nodes+{%0A++++++id%2C%0A++++++is_verified%2C%0A++++++followed_by_viewer%2C%0A++++++requested_by_viewer%2C%0A++++++full_name%2C%0A++++++profile_pic_url%2C%0A++++++username%0A++++}%0A++}%0A}%0A&ref=relationships%3A%3Afollow_list";
-		var xmlhttp;
+		var apiCall = "https://www.instagram.com/query/";
 		
+		var apiParams = "q=ig_user("+this.userId+") ";
+		if (this.cursor == "") {
+			apiParams += "{ follows.first(20) { count, page_info, nodes { id,is_verified,followed_by_viewer,requested_by_viewer,full_name,profile_pic_url,username } } }";
+		} else {
+			apiParams += "{ follows.after(" + this.cursor +",20) { count, page_info, nodes { id,is_verified,followed_by_viewer,requested_by_viewer,full_name,profile_pic_url,username } } }";
+		}
+		apiParams += "&ref=relationships::follow_list";
+	
+		var xmlhttp;	
 		xmlhttp = new XMLHttpRequest();
 		xmlhttp.owner = this;
 		xmlhttp.onreadystatechange = function() {
@@ -64,39 +87,53 @@ var FollowToy = {
 				//println(xmlhttp.responseText);
 				
 				if (data.follows != undefined && data.follows.nodes.length > 0) {
-					println("Following: " + data.follows.nodes.length);
+					// println("Following: " + data.follows.nodes.length);
 					for (i = 0; i<data.follows.nodes.length; i++) {
 						var followedUser = data.follows.nodes[i];
 						this.owner.following.push(followedUser);
 						// println("following: " + followedUser.username);
 					}
+					if (data.follows.page_info.has_next_page) {
+						this.owner.cursor = data.follows.page_info.end_cursor;
+						this.owner.fetchFollowing();
+					} else {
+						this.owner.cursor = "END";
+						println("Following count: " + this.owner.following.length);
+					}
 					
-					// sort alphabetically by username
-					this.owner.following.sort(function(a,b) { return a.username.charCodeAt(0) - b.username.charCodeAt(0)});
-					
-					// create the report element <div> and inject it into the page
-					var report = this.owner.createReport();
-					var articles = document.getElementsByTagName("article");
-					articles[0].insertBefore(report, articles[0].lastChild);
-					
-					// add our menu item into the profile header
-					var headers = document.getElementsByTagName("header");
-					var showLink = document.createElement("a");
-					showLink.setAttribute("href", "#")					
-					showLink.setAttribute("onclick", "document.getElementById('followToyReport').setAttribute('style', 'display: block')");
-					showLink.appendChild(document.createTextNode("👀 FollowToy"));
-					var linkItem = document.createElement("li");
-					linkItem.setAttribute("style", "line-height: 1.2em");
-					linkItem.appendChild(showLink);
-					headers[0].getElementsByTagName("ul")[0].appendChild(linkItem);
+					if (this.owner.cursor == "END") {
+						// sort alphabetically by username
+						this.owner.following.sort(function(a,b) { return a.username.charCodeAt(0) - b.username.charCodeAt(0)});
+						
+						// create the report element <div> and inject it into the page
+						var report = this.owner.createReport();
+						var articles = document.getElementsByTagName("article");
+						articles[0].insertBefore(report, articles[0].lastChild);
+						
+						// add our menu item into the profile header
+						var headers = document.getElementsByTagName("header");
+						var showLink = document.createElement("a");
+						showLink.setAttribute("href", "#")					
+						showLink.setAttribute("onclick", "document.getElementById('followToyReport').setAttribute('style', 'display: block')");
+						showLink.appendChild(document.createTextNode("👀 FollowToy"));
+						var linkItem = document.createElement("li");
+						linkItem.setAttribute("style", "line-height: 1.2em");
+						linkItem.appendChild(showLink);
+						headers[0].getElementsByTagName("ul")[0].appendChild(linkItem);
+						this.owner.cursor == "";
+					}
 					
 				} else {
 					println("Error: " + xmlhttp.responseText);
 				}
 			}
 		};
-		xmlhttp.open("GET", apiCall, true);
-		xmlhttp.send();
+		xmlhttp.open("POST", apiCall, true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.setRequestHeader("X-Instagram-AJAX", "1");
+		xmlhttp.setRequestHeader("X-CSRFToken", this.requestToken);
+		
+		xmlhttp.send(apiParams);
 	},
 	
 	createReport: function() {
@@ -189,6 +226,7 @@ document.onreadystatechange = function() {
 	setTimeout(function() {
 		var followToy = Object.create(FollowToy);
 		followToy.userId = getCookie("ds_user_id");
+		followToy.requestToken = getCookie("csrftoken");
 		
 		println(followToy.version + " loaded.");
 			
